d8e4095bab2c8f60d73649044ed071ce2f9a7fe0
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-07-18 00:21:58 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,38 +3,41 @@ import React from 'react';
 // var ReactDom = require('react-dom');
 import ReactDom from 'react-dom';
 
-// ReactDom.render(<h1>Hello World</h1>, document.getElementById('root')); 
+// this will show error (Adjacent JSX elements must be wrapped in an enclosing tag)
+
+// ReactDom.render(
+//     <h1>Hello World</h1>
+//     <p>Mayank Singh Chouhan</p>,
+
+//     document.getElementById('root')
+// ); 
 
 
 
+// we can wrape adjacent JSX element in a div to solve this problem
 
-// babel will turn the above code into the below code
-// You can also notice why importing react modeule is important
-// now a days babel can import on its own
+// ReactDom.render(
+//     <div>
+//         <h1>Hello World</h1>
+//         <h2>Mayank Singh Chouhan</h2>
+//         <p>Can render any number of elements this way</p>
+//     </div>,
 
-
-import { jsx as _jsx } from "react/jsx-runtime";
-
-ReactDom.render(_jsx("h1", {
-    children: "Hello World"
-}), document.getElementById('root'));
-    
-    
-// you don't even need to import react any more in this file babel will do it automatically
-// visit this(https://legacy.reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html) link to know more
-    
-// our original code did not need to import React to use JSX anymore! 
-// (But we would still need to import React in order to use Hooks or other exports that React provides.)
+//     document.getElementById('root')
+// ); 
 
 
 
+// In react v16 and above it is possible 
+// for render to return an array of elements
+// const names = ["Mayank", "Singh", "Chouhan"]
 
-// how to do above in pure javascript(no react no nothing)
+ReactDom.render(
+    [
+        <h1>Hello World</h1>,
+        <h2>Mayank Singh Chouhan</h2>,
+        <p>Can render any number of elements this way</p>
+    ],
 
-
-var h1 = document.createElement("h1");
-h1.innerHTML = "Maynk Singh Chouhan";
-document.getElementById("root").appendChild(h1);
-
-
-
+    document.getElementById('root')
+); 
