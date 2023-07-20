7791ffae04a2bba738bad50982ede23a2d3158f3
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-07-20 18:40:25 +0530

    done

--- src/index.js
+++ src/index.js
@@ -3,41 +3,27 @@ import React from 'react';
 // var ReactDom = require('react-dom');
 import ReactDom from 'react-dom';
 
-// this will show error (Adjacent JSX elements must be wrapped in an enclosing tag)
+// preferred method of wrapping adjacent elements(better than methods explored in previous branch)
 
 // ReactDom.render(
-//     <h1>Hello World</h1>
-//     <p>Mayank Singh Chouhan</p>,
-
-//     document.getElementById('root')
-// ); 
-
-
-
-// we can wrape adjacent JSX element in a div to solve this problem
-
-// ReactDom.render(
-//     <div>
+//     <React.Fragment>
 //         <h1>Hello World</h1>
 //         <h2>Mayank Singh Chouhan</h2>
 //         <p>Can render any number of elements this way</p>
-//     </div>,
+//     </React.Fragment>,
 
 //     document.getElementById('root')
 // ); 
 
 
-
-// In react v16 and above it is possible 
-// for render to return an array of elements
-// const names = ["Mayank", "Singh", "Chouhan"]
+// syntactic sugar(for react fragment)
 
 ReactDom.render(
-    [
-        <h1>Hello World</h1>,
-        <h2>Mayank Singh Chouhan</h2>,
+    <>
+        <h1>Hello World</h1>
+        <h2>Mayank Singh Chouhan</h2>
         <p>Can render any number of elements this way</p>
-    ],
+    </>,
 
     document.getElementById('root')
 ); 
