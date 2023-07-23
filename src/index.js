45eb8c313bde3b3bbcd24ced834c123711f23ce5
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-07-24 00:10:48 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// if you want to write an expression inside jsx you can do it 
+// you just have to write in inside {}
+
+const name = "Mayank Singh Chouhan";
+
 ReactDOM.render(
     <>
-        <h1>Mayank Singh Chouhan Netflix Pick</h1>
-        <p>List of 5 Best Series.</p>
-        <ol>
-            <li>Dark</li>
-            <li>Extra Curricular</li>
-            <li>My Holo Love</li>
-            <li>My first-2 love</li>
-            <li>Mr Robot</li>
-        </ol>
+        <h1>My name is {name}</h1>
+        <p>My lucky number is {10 + 2}</p>
+
     </>, document.getElementById("root")
 );
+
+
+// you can only do this with expressions not with statements
+// following will give an error
+
+// ReactDOM.render(
+//     <>
+//         <h1>My name is {name}</h1>
+//         <p>My lucky number is {
+//             if(1 < 0) {
+//                 console.log("YES");
+//             } else {
+//                 console.log("NO");
+//             }
+//         }</p>
+
+//     </>, document.getElementById("root")
+// );
