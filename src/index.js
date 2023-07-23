9dfc4c3e80b5ca0e4e2c6920649f69f882ed71ee
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-07-24 01:36:48 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// if you want to write an expression inside jsx you can do it 
-// you just have to write in inside {}
 
-const name = "Mayank Singh Chouhan";
+const fname = "Mayank";
+const mname = "Singh";
+const lname = "Chouhan";
+
+// ReactDOM.render(
+//     <>
+//         <h1>My name is {fname} {mname} {lname}</h1>
+//         <p>My lucky number is {10 + 2}</p>
+
+//     </>, document.getElementById("root")
+// );
+
+
+
+// ReactDOM.render(
+//     <>
+//         <h1>My name is {fname + " " + mname + " " + lname}</h1>
+//         <p>My lucky number is {10 + 2}</p>
+
+//     </>, document.getElementById("root")
+// );
+
+
+
+// preferred and most efficient method
 
 ReactDOM.render(
     <>
-        <h1>My name is {name}</h1>
+        <h1>{`My first name is ${fname} ${mname} ${lname}`}</h1>
         <p>My lucky number is {10 + 2}</p>
 
     </>, document.getElementById("root")
 );
-
-
-// you can only do this with expressions not with statements
-// following will give an error
-
-// ReactDOM.render(
-//     <>
-//         <h1>My name is {name}</h1>
-//         <p>My lucky number is {
-//             if(1 < 0) {
-//                 console.log("YES");
-//             } else {
-//                 console.log("NO");
-//             }
-//         }</p>
-
-//     </>, document.getElementById("root")
-// );
