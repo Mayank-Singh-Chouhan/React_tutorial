00aef395cd926f892d4234848c221d379eb69e6d
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-07-25 19:42:06 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-const fname = "Mayank";
-const mname = "Singh";
-const lname = "Chouhan";
-
-// ReactDOM.render(
-//     <>
-//         <h1>My name is {fname} {mname} {lname}</h1>
-//         <p>My lucky number is {10 + 2}</p>
-
-//     </>, document.getElementById("root")
-// );
-
-
-
-// ReactDOM.render(
-//     <>
-//         <h1>My name is {fname + " " + mname + " " + lname}</h1>
-//         <p>My lucky number is {10 + 2}</p>
-
-//     </>, document.getElementById("root")
-// );
-
-
-
-// preferred and most efficient method
+const name = "Mayank Singh Chouhan";
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
 
 ReactDOM.render(
     <>
-        <h1>{`My first name is ${fname} ${mname} ${lname}`}</h1>
-        <p>My lucky number is {10 + 2}</p>
+        <h1>Hello, My name is {name}</h1>
+        <p>Date - {date}</p>
+        <p>Time - {time}</p>
 
     </>, document.getElementById("root")
 );
