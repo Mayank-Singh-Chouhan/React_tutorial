2e842e4d52a19d44e41febf0a8d6382533583f62
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-07-30 22:25:49 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Mayank Singh Chouhan";
-const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const link = "https://www.thapatechnical.com/";
 
 ReactDOM.render(
     <>
-        <h1>Hello, My name is {name}</h1>
-        <p>Date - {date}</p>
-        <p>Time - {time}</p>
+        <h1 contentEditable="true">My name is {name}</h1>
+        <img src={img1} alt="Random Image"/>
+        <img src={img2} alt="Random Image"/>
+
+        <a href={link} target="_blank">
+            <img src={img3} alt="Random Image"/>
+        </a>
 
     </>, document.getElementById("root")
 );
