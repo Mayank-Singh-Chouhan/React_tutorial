d07a2c4b7f5d6333579df7d5752a6866c07db561
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-02 19:09:33 +0530

    done

---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+    return (
+        <ol>
+            <li>Mayank</li>
+            <li>Singh</li>
+            <li>Chouhan</li>
+            <li>Mayank Singh Chouhan</li>
+            <li>Plz Eat</li>
+        </ol>
+    );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
+import App from "./App";
 
-let currDate = new Date(2023, 8, 2, 5);
-currDate = currDate.getHours();
-
-let greeting = "";
-let style = {};
-
-if (currDate >= 1 && currDate < 12) {
-    greeting = "Good Morning";
-    style.color = "#ADD8E6";
-} else if (currDate >= 12 && currDate < 19) {
-    greeting = "Good Afternoon";
-    style.color = "orange";
-} else {
-    greeting = "Good Night";
-    style.color = "#00008b";
-}
-
-ReactDOM.render(
-    <>
-        <div>
-            <h1>
-                Hello sir, <span style={style}>{greeting}</span>
-            </h1>
-        </div>
-
-    </>, document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
