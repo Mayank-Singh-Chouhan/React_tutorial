8fb2d077839c06d7830004800a7732c4251a6d38
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-02 23:25:12 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import React from "react";
+const youtuber = "Thapa Technical";
+const favprog = "React JS";
 
-function App() {
-    let currDate = new Date();
-    currDate = currDate.getHours();
-
-    let greeting = "";
-    let style = {};
-
-    if (currDate >= 1 && currDate < 12) {
-        greeting = "Good Morning";
-        style.color = "#ADD8E6";
-    } else if (currDate >= 12 && currDate < 19) {
-        greeting = "Good Afternoon";
-        style.color = "orange";
-    } else {
-        greeting = "Good Night";
-        style.color = "#00008b";
-    }
-
-    return (
-        <>
-            <div>
-                <h1>
-                    Hello sir, <span style={style}>{greeting}</span>
-                </h1>
-            </div>
-        </>
-    );
+function myName() {
+    let name = "Vinod";
+    return name;
 }
 
-export default App;
+function myNames() {
+    let names = "Vinod Thapa";
+    return names;
+}
+
+export default youtuber;
+// export { favprog };
+export { favprog, myName, myNames };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import youtuber from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// you can use any name when importing default export
+// import myfav from "./App"; 
+
+// use the exact same name of for other(non default) export as in the file or you will get error
+// import myfav, { favprog } from "./App";
+import myfav, { favprog, myName, myNames } from "./App";
+
+// if you wanna import all the exports from a file you can do this
+// import * as ques from "./App";
+
+
+
+
+ReactDOM.render(
+    <>
+        <ol>
+            <li>Thapa</li>
+            {/* <li>{youtuber}</li> */}
+            <li>{myfav}</li>
+            <li>{favprog}</li>
+            <li>{myName()}</li>
+            <li>{myNames()}</li>
+        </ol>
+
+        {/* <ol>
+            <li>{ques.default}</li>
+            <li>{ques.favprog}</li>
+            <li>{ques.myName()}</li>
+            <li>{ques.myNames()}</li>
+        </ol> */}
+
+    </>, document.getElementById("root")
+);
