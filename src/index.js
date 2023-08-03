a64c7fcae7fc7c6337e50f62dcdbbfbbe34c2d3f
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-03 17:05:39 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Card from "./Cards";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <>
+        <Card
+            imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+            title="A Netflix Original Series"
+            sname="DARK"
+            link="https://www.netflix.com/in/title/80100172"
+        />
+        <Card
+            imgsrc="https://i.pinimg.com/originals/9f/45/88/9f4588f879aa9781cbb77e93c6cef20a.jpg"
+            title="A Netflix Original Series"
+            sname="Extracurricular"
+            link="https://www.netflix.com/in/title/80990668"
+        />
+        <Card
+            imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
+            title="A Netflix Original Series"
+            sname="Stranger Things"
+            link="https://www.netflix.com/in/title/80057281"
+        />
+
+    </>, document.getElementById("root")
+);
