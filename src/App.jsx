91de7278846c42ddc07afa495cf35eae29ca32d9
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-05 18:23:21 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import Netflix from "./Netflix";
 import Amazon from "./Amazon";
 
-let favSeries = "amazon";
+let favSeries = "netflix";
 
-const FavS = () => {
-    if (favSeries === "netflix") {
-        return <Netflix />;
-    } else {
-        return <Amazon />;
-    }
-}
+// const FavS = () => {
+//     if (favSeries === "netflix") {
+//         return <Netflix />;
+//     } else {
+//         return <Amazon />;
+//     }
+// }
 
 const App = () => {
     return (
         <>
             <h1 className="heading_style">List Of Top 5 Netflix Series In 2020</h1>
-            <FavS />
+
+            {/* <FavS /> */}
+
+            {(favSeries === "netflix") ? <Netflix /> : <Amazon />}
         </>
     );
 }
