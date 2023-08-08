16a97f7a6da96dcc6896de4a48fe86b9f39beb0f
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-08 15:24:12 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
     const [fullName, setFullName] = useState(
         {
             fname: "",
-            lname: ""
+            lname: "",
+            email: "",
+            phone: ""
         }
     );
 
@@ -12,19 +14,39 @@ const App = () => {
         // console.log(event.target.value);
         // console.log(event.target.name);
 
-        const value = event.target.value;
-        const name = event.target.name;
+        // const value = event.target.value;
+        // const name = event.target.name;
+
+        const { value, name } = event.target;
 
         setFullName((preValue) => {
             if (name === "fName") {
                 return {
                     fname: value,
-                    lname: preValue.lname
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: preValue.phone
                 };
             } else if (name === "lName") {
                 return {
                     fname: preValue.fname,
-                    lname: value
+                    lname: value,
+                    email: preValue.email,
+                    phone: preValue.phone
+                };
+            } else if (name === "email") {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: value,
+                    phone: preValue.phone
+                };
+            } else if (name === "phone") {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email: preValue.email,
+                    phone: value
                 };
             }
         });
@@ -32,6 +54,7 @@ const App = () => {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        alert("form submitted");
     }
 
     return (
@@ -41,6 +64,9 @@ const App = () => {
 
                     <div>
                         <h1> Hello {fullName.fname} {fullName.lname} </h1>
+                        <p> {fullName.email} </p>
+                        <p> {fullName.phone} </p>
+
                         <input type="text"
                             placeholder="Enter Your Name"
                             name="fName"
@@ -55,6 +81,24 @@ const App = () => {
                             onChange={inputEvent}
                             value={fullName.lname}
                         />
+                        <br />
+                        <input
+                            type="email"
+                            placeholder="Enter Your Email"
+                            name="email"
+                            onChange={inputEvent}
+                            value={fullName.email}
+                            autoComplete="off"
+                        />
+                        <br />
+                        <input
+                            type="number"
+                            placeholder="Enter Your Phone No."
+                            name="phone"
+                            onChange={inputEvent}
+                            value={fullName.phone}
+                        />
+
                         <button type="submit"> Submit Me 👍</button>
                     </div>
 
