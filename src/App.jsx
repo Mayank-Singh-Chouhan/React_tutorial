a7ae740fc163b4f3015ec8e1e842c9f0777a9f4b
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-09 16:25:15 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,42 @@
 import React, { useState } from "react";
 
+// controlled component
 const App = () => {
-    const [fullName, setFullName] = useState(
-        {
-            fname: "",
-            lname: "",
-            email: "",
-            phone: "",
-            qua: ""
+    const [name, setName] = useState("");
+    const [pswd, setPswd] = useState("");
+
+    const handleChange = (e) => {
+        // console.log(e.target.name);
+        if (e.target.name === "firstName") {
+            const capName = (e.target.value).toUpperCase();
+            setName(capName);
+        } else {
+            setPswd(e.target.value);
         }
-    );
-
-    const inputEvent = (event) => {
-        // console.log(event.target.value);
-        // console.log(event.target.name);
-
-        // const value = event.target.value;
-        // const name = event.target.name;
-
-        const { value, name } = event.target;
-
-        setFullName((preValue) => {
-            // console.log(preValue);
-
-            return {
-                ...preValue,
-                [name]: value
-            }
-
-            // if (name === "fName") {
-            //     return {
-            //         fname: value,
-            //         lname: preValue.lname,
-            //         email: preValue.email,
-            //         phone: preValue.phone
-            //     };
-            // } else if (name === "lName") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: value,
-            //         email: preValue.email,
-            //         phone: preValue.phone
-            //     };
-            // } else if (name === "email") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: preValue.lname,
-            //         email: value,
-            //         phone: preValue.phone
-            //     };
-            // } else if (name === "phone") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: preValue.lname,
-            //         email: preValue.email,
-            //         phone: value
-            //     };
-            // }
-        });
     }
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        alert("form submitted");
-    }
+    // const handlePassword = (e) => {
+    //     console.log(e.target.value);
+    //     setPswd(e.target.value);
+    // }
 
     return (
         <>
-            <div className="main_div">
-                <form onSubmit={onSubmit}>
+            <form>
+                <label> First Name :</label>
+                <br />
+                <input type="text" name="firstName" value={name} onChange={handleChange} />
+                <br />
+                <label> Password :</label>
+                <br />
+                {/* <input type="text" value={pswd} onChange={handlePassword} /> */}
+                <input type="text" name="password" value={pswd} onChange={handleChange} />
+                <br />
+            </form>
 
-                    <div>
-                        <h1> Hello {fullName.fname} {fullName.lname} </h1>
-                        <p> {fullName.email} </p>
-                        <p> {fullName.phone} </p>
-                        <p> {fullName.qua} </p>
-
-                        <input type="text"
-                            placeholder="Enter Your Name"
-                            name="fname"
-                            onChange={inputEvent}
-                            value={fullName.fname}
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            placeholder="Enter Your Last Name"
-                            name="lname"
-                            onChange={inputEvent}
-                            value={fullName.lname}
-                        />
-                        <br />
-                        <input
-                            type="email"
-                            placeholder="Enter Your Email"
-                            name="email"
-                            onChange={inputEvent}
-                            value={fullName.email}
-                            autoComplete="off"
-                        />
-                        <br />
-                        <input
-                            type="number"
-                            placeholder="Enter Your Phone No."
-                            name="phone"
-                            onChange={inputEvent}
-                            value={fullName.phone}
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            placeholder="Enter Your Qualification"
-                            name="qua"
-                            onChange={inputEvent}
-                            value={fullName.qua}
-                        />
-
-                        <button type="submit"> Submit Me 👍</button>
-                    </div>
-
-                </form>
-            </div>
         </>
     );
+
 }
 
 export default App;
