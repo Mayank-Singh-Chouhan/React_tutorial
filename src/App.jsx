e1f75c2fb1fd86cc7ce87a8c7d26bdeaf1a227b9
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-11 23:38:04 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,125 +1,50 @@
 import React, { useState } from "react";
+import ToDoList from "./ToDoList";
 
 const App = () => {
-    const [fullName, setFullName] = useState(
-        {
-            fname: "",
-            lname: "",
-            email: "",
-            phone: "",
-            qua: ""
-        }
-    );
+    const [inputList, setInputList] = useState("");
+    const [items, setItems] = useState([]);
 
-    const inputEvent = (event) => {
-        // console.log(event.target.value);
-        // console.log(event.target.name);
-
-        // const value = event.target.value;
-        // const name = event.target.name;
-
-        const { value, name } = event.target;
-
-        setFullName((preValue) => {
-            // console.log(preValue);
-
-            return {
-                ...preValue,
-                [name]: value
-            }
-
-            // if (name === "fName") {
-            //     return {
-            //         fname: value,
-            //         lname: preValue.lname,
-            //         email: preValue.email,
-            //         phone: preValue.phone
-            //     };
-            // } else if (name === "lName") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: value,
-            //         email: preValue.email,
-            //         phone: preValue.phone
-            //     };
-            // } else if (name === "email") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: preValue.lname,
-            //         email: value,
-            //         phone: preValue.phone
-            //     };
-            // } else if (name === "phone") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: preValue.lname,
-            //         email: preValue.email,
-            //         phone: value
-            //     };
-            // }
-        });
+    const itemEvent = (event) => {
+        setInputList(event.target.value);
     }
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        alert("form submitted");
+    const listOfItems = () => {
+        setItems((oldItems) => {
+            return [...oldItems, inputList]
+        });
+        setInputList("");
+    }
+
+    const deleteItems = (id) => {
+        setItems((oldItems) => {
+            return oldItems.filter((arrElem, index) => {
+                return index !== id;
+            });
+        });
     }
 
     return (
         <>
             <div className="main_div">
-                <form onSubmit={onSubmit}>
+                <div className="center_div">
+                    <br />
+                    <h1>ToDo List</h1>
+                    <br />
+                    <input type="text" value={inputList} placeholder="Add a task" onChange={itemEvent} />
+                    <button onClick={listOfItems}>+</button>
 
-                    <div>
-                        <h1> Hello {fullName.fname} {fullName.lname} </h1>
-                        <p> {fullName.email} </p>
-                        <p> {fullName.phone} </p>
-                        <p> {fullName.qua} </p>
-
-                        <input type="text"
-                            placeholder="Enter Your Name"
-                            name="fname"
-                            onChange={inputEvent}
-                            value={fullName.fname}
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            placeholder="Enter Your Last Name"
-                            name="lname"
-                            onChange={inputEvent}
-                            value={fullName.lname}
-                        />
-                        <br />
-                        <input
-                            type="email"
-                            placeholder="Enter Your Email"
-                            name="email"
-                            onChange={inputEvent}
-                            value={fullName.email}
-                            autoComplete="off"
-                        />
-                        <br />
-                        <input
-                            type="number"
-                            placeholder="Enter Your Phone No."
-                            name="phone"
-                            onChange={inputEvent}
-                            value={fullName.phone}
-                        />
-                        <br />
-                        <input
-                            type="text"
-                            placeholder="Enter Your Qualification"
-                            name="qua"
-                            onChange={inputEvent}
-                            value={fullName.qua}
-                        />
-
-                        <button type="submit"> Submit Me 👍</button>
-                    </div>
-
-                </form>
+                    <ol>
+                        {items.map((itemval, index) => {
+                            return <ToDoList
+                                key={index}
+                                text={itemval}
+                                id={index}
+                                onSelect={deleteItems}
+                            />;
+                        })}
+                    </ol>
+                </div>
             </div>
         </>
     );
