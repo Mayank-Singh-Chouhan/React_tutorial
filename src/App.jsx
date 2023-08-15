49aef126d9ef76eeb4a95181ec342a4fde209730
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2023-08-15 13:44:29 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import Child from "./Child";
 
-// controlled component
 const App = () => {
-    const [name, setName] = useState("");
-    const [pswd, setPswd] = useState("");
 
-    const handleChange = (e) => {
-        // console.log(e.target.name);
-        if (e.target.name === "firstName") {
-            const capName = (e.target.value).toUpperCase();
-            setName(capName);
-        } else {
-            setPswd(e.target.value);
-        }
+    const getData = (data) => {
+        console.log(data);
     }
-
-    // const handlePassword = (e) => {
-    //     console.log(e.target.value);
-    //     setPswd(e.target.value);
-    // }
 
     return (
         <>
-            <form>
-                <label> First Name :</label>
-                <br />
-                <input type="text" name="firstName" value={name} onChange={handleChange} />
-                <br />
-                <label> Password :</label>
-                <br />
-                {/* <input type="text" value={pswd} onChange={handlePassword} /> */}
-                <input type="text" name="password" value={pswd} onChange={handleChange} />
-                <br />
-            </form>
-
+            <Child getData={getData} />
         </>
     );
-
 }
 
 export default App;
